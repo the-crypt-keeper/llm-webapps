bfd9b0ae388e55597d1e649437dc8d573ef3e6af
Mike <mike@cannstandard.om>
2023-06-16 12:19:59 -0400

    as working as v1 is going to get

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 import os
 import sys
 import glob
-import html
+import re
 
 # Parse command-line arguments.
 if len(sys.argv) > 1:
@@ -48,8 +48,23 @@ elif selected_env == 'HTML_jQuery':
     components.html(filebody, height=400, scrolling=True)
     st.code(filebody)
 else:
+    # babel-standalone chokes on imports and export - strip them out
     hacked_js = '\n'.join([x for x in filebody.split('\n') if not 'export default' in x and not 'import' in x])
-    #entrypoint = function App() {
+
+    # figure out the entrypoint
+    function_regex = r"function\**\s+(\S*)\s*\((.*)\)(.*){"
+    arrow_function_regex = r"\s+(\S*)\s*=\s*\((.*)\)(.*)\s*=>\s*{"
+
+    matches = re.findall(function_regex, filebody)
+    if len(matches) == 0:
+        matches = re.findall(arrow_function_regex, filebody)
+    if len(matches) == 0:
+        # fallback
+        entry = 'App'
+    else:
+        entry = matches[0][0]
+    
+    # babel-standalone magic to transpile JSX in the browser
     react_template = f"""
     <body>
         <div id="root"></div>
@@ -63,7 +78,7 @@ else:
           const {{ useState }} = React;
           {hacked_js}
 
-          ReactDOM.render(<App />, document.getElementById('root'));
+          ReactDOM.render(<{entry} />, document.getElementById('root'));
         </script>
     </body>
     """
